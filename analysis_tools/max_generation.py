cbec4767f69ad6ee28eb36e2c10faa097343cd14
Alexander Swenson <aaswenson@wisc.edu>
2017-09-14 13:55:23 -0500

    initial file for thermal analysis

---
+++ analysis_tools/max_generation.py
@@ -0,0 +1,11 @@
+""" This module determines maximum heat generation in a sCO2 reactor fuel pin
+as a function of pitch to diameter ratio.
+
+The following functions are contained in this module:
+
+"""
+
+def get_thermal_cond(mat, T):
+    """Estimate thermal conductivity of given material.
+    """
+
