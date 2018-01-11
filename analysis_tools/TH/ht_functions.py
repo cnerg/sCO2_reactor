3d3fc42e5f9692412cdb5e218d7b3e30734156eb
Alexander Swenson <aaswenson@wisc.edu>
2018-01-11 13:35:29 -0600

    switching data structures to prep for switch to quadratic iteration scheme

--- analysis_tools/TH/ht_functions.py
+++ analysis_tools/TH/ht_functions.py
@@ -25,7 +25,9 @@ class FlowIteration:
     """
     # geometric attributes
     r_channel = 0; PD = 0; c = 0; L = 0; Vol_fuel = 0;
-    guess = 0; N_channels = 0; A_flow = 0; A_fuel = 0;
+    guess = np.array([1, 10, 20]); N_channels = np.zeros(3); error = np.zeros(3)
+    guess_idx = 0
+    A_flow = 0; A_fuel = 0;
     mass = 0
     # temperature drop
     dt = T_centerline - T_bulk
@@ -42,7 +44,7 @@ class FlowIteration:
         self.c = c
         self.pitch = (self.r_channel + self.c) * PD * 2
         self.L = L
-        self.guess = guess
+#        self.guess = guess
         self.iterations = 0
 
     def check_converge(self):
@@ -66,7 +68,7 @@ class FlowIteration:
         self.A_flow = self.r_channel ** 2 * math.pi
         self.A_fuel = math.sqrt(3)*self.pitch**2 / 2.0 -\
                       (self.r_channel + self.c) ** 2 * math.pi
-        self.G_dot = m_dot / (self.A_flow * self.guess)
+        self.G_dot = m_dot / (self.A_flow * self.guess[self.guess_idx])
         self.D_e = 2.0 * self.r_channel
         self.v = self.G_dot / rho_cool
 
@@ -106,9 +108,9 @@ class FlowIteration:
         self.q_per_channel = 2 * q_trip_max * self.A_fuel * self.L / math.pi
         
         # calculate required number of channels
-        self.N_channels = math.ceil(Q_therm / self.q_per_channel)
+        self.N_channels[self.guess_idx] = math.ceil(Q_therm / self.q_per_channel)
         # calculate total fuel volume and q_bar
-        self.Vol_fuel = self.A_fuel * self.L * self.N_channels
+        self.Vol_fuel = self.A_fuel * self.L * self.N_channels[self.guess_idx]
         self.q_bar = Q_therm / self.Vol_fuel
         
     def calc_dp(self):
@@ -118,21 +120,26 @@ class FlowIteration:
         f = 0.184 / math.pow(self.Re, 0.2)
         # Darcy pressure drop (El-Wakil 9-3)
         self.dp = f * self.L * rho_cool * self.v * self.v / (2*self.D_e)
+
+    def fit_error(self):
+        """
+        """
     
     def Iterate(self):
         """Perform Flow Calc Iteration
         """
-        converge = False
-        while converge == False:
-            # perform necessary physics calculations
+        for i in range(0,3):
+            self.guess_idx = i
             self.mass_flux_channel()
             self.calc_nondim()
             self.get_h_bar()
             self.get_q_bar(Q_therm)
             self.calc_dp()
-            converge = self.check_converge()
-            self.guess = self.N_channels
-            self.iterations += 1
+
+        self.error = abs(self.guess - self.N_channels)
+        print(self.error)
+
+
 
     def calc_reactor_mass(self):
         """Based on results of the iteration, calculate the reactor mass.
