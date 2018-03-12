53b22c318be700707277274f4a7078bdc1de1b78
Alexander Swenson <aaswenson@wisc.edu>
2018-03-12 15:25:58 -0500

    intermediate broken commit, for move to apollo

--- optimization/neutronics/mcnp_inputs.py
+++ optimization/neutronics/mcnp_inputs.py
@@ -5,6 +5,17 @@ import sys
 import argparse
 from base_input import base_string
 
+def merge_comps(compA, compB):
+    """
+    """
+    for isotope in compB:
+        if isotope in compA.keys():
+            compA[isotope] += compB[isotope]
+        else:
+            compA.update({isotope : compB[isotope]})
+
+    return compA
+
 class HomogeneousInput:
     """Class to write MCNP input files for pin cell modeling in an infinite
     lattice (in the x-y directions).
@@ -31,8 +42,20 @@ class HomogeneousInput:
         clad_to_cool = 0.127844                #
         frac_cool = 1 - frac_fuel              #
         frac_clad = frac_cool * clad_to_cool   #
+        uran_frac = 0.6 # UN in tungsten       #
         ########################################
+        # densities
+        rho_W = 19.3
+        rho_cool = 87.8
+        rho_UN = 11.3 # g/cc
+        rho_In = 8.19
         
+        mass_fuel = frac_fuel * uran_frac * rho_UN
+        mass_matr = frac_fuel * (1 - uran_frac) * rho_W
+        mass_cool = frac_cool * rho_cool
+        mass_clad = frac_clad * rho_In
+        mass_total = mass_fuel + mass_matr + mass_cool + mass_clad
+
         # fuel mass fractions
         U_fraction = {'U' : 0.94441, 'N' : 0.05559}
         Uran_comp =  {'235' : enrich, '238' : 1 - enrich}
@@ -40,6 +63,13 @@ class HomogeneousInput:
         fuel_comp = {7015 : U_fraction['N'],
                      92235 : U_fraction['U'] * Uran_comp['235'],
                      92238 : U_fraction['U'] * Uran_comp['238']
+                    }
+
+        matr_comp = {74180 : 1.1746e-03,
+                     74182 : 2.6227e-01,
+                     74183 : 1.4241e-01,
+                     74184 : 3.0658e-01,
+                     74186 : 2.8757e-01
                     }
 
         clad_comp = {6000   : 0.00073,
@@ -61,17 +91,36 @@ class HomogeneousInput:
         cool_comp = {6000 : 0.272912,
                      8016 : 0.727088
                     }
+
+
+        components = [fuel_comp, matr_comp, clad_comp, cool_comp]
         
+        # get isotopic masses for fuel, matrix, coolant, cladding
+        print(fuel_comp)
+        fuel_comp = {isotope : fuel_comp[isotope] 
+                     * mass_fuel for isotope in fuel_comp}
+        print(fuel_comp)
+        matr_comp = {isotope : matr_comp[isotope] 
+                     * mass_matr for isotope in matr_comp}
+        clad_comp = {isotope : clad_comp[isotope]
+                     * mass_clad for isotope in clad_comp}
+        cool_comp = {isotope : cool_comp[isotope]
+                     * mass_cool for isotope in cool_comp}
 
+        homog_comp = {}
+        for frac in components:
+            homog_comp = merge_comps(homog_comp, frac)
+        
+        # write the mcnp string
+        self.fuel_string = "\
+m1\n"
+        for isotope in sorted(homog_comp.keys()):
+            self.fuel_string += "    {0} -{1}\n".format(isotope,
+                    round(homog_comp[isotope] / mass_total, 7))
 
-        fuel_mat = "\
-m1
-    7015 -{0}
-    92235 -{1}
-    92238 -{2}".format(fuel_comp['N'], 
-                       fuel_comp['U'] * Uran_comp['235'],
-                       fuel_comp['U'] * Uran_comp['238'])
+        print(self.fuel_string)
 
+        
 
     def write_input(self):
         """ Write MCNP6 input files.
@@ -96,16 +145,5 @@ m1
         return filename
 
 if __name__ == '__main__':
-    
-    tarball = tarfile.open('radius_sweep.tar.gz', 'w|gz')
-    input_list = open('input_list.txt', 'w')
-     
-    for radius in range(5, 45, 1):
-        inp = HomogeneousInput(radius, 15)
-        print(radius)
-        infilename = inp.write_input()
-        input_list.write(infilename+'\n')
-        tarball.add(infilename)
-
-    input_list.close()
-    tarball.add('input_list.txt')
+   test = HomogeneousInput(15,15)
+   test.homogenize_fuel_clad_cool(0.6)
