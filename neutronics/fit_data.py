677fa4eedd388f08bf6709c05192744d7abdf7d1
Alexander Swenson <aaswenson@wisc.edu>
2018-04-16 10:30:54 -0500

    ND interpolation of the data

--- neutronics/fit_data.py
+++ neutronics/fit_data.py
@@ -1,4 +1,5 @@
 from scipy import optimize
+from scipy.interpolate import NearestNDInterpolator
 import numpy as np
 import math
 import scipy.linalg
@@ -10,24 +11,12 @@ def load_from_csv(datafile="depl_results.csv"):
     """load the results data from a csv.
     """
     data = np.genfromtxt(datafile, delimiter=',',
-            names=list(ns.parameters.keys()) + ['keff', 'ave_E'])
+            names=ns.dimensions + ['keff', 'ave_E', 'mass'])
     
     return data
 
 data = load_from_csv()
 
-def func(data, a, b, c, d, e, f):
-    """
-    """
-    return a*data[0]**2 + b*data[1]**2 + c*data[0]*data[1] + d*data[0] + e*data[1] * f
+intpl = NearestNDInterpolator((data['mass'], data['enrich'], data['PD']), data['keff'])
 
-guess = (1,1,1,1,1,1)
-C, pcov = optimize.curve_fit(func, (data['core_r'], data['PD']), data['keff'], guess)
-
-X = data['core_r'] 
-Y = data['PD']
-
-Z = C[0]*X**2 + C[1]*Y**2 + C[2]*X*Y + C[3]*X + C[4]*Y + C[5]
-
-
-print(np.mean(abs(Z - data['keff'])))
+print(intpl(1431, 0.87583, 1.53677))
