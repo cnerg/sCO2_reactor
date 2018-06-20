82d4db71c6c13b53eb20b35698ac51dae901ce81
Alexander Swenson <aaswenson@wisc.edu>
2018-06-20 11:16:05 -0500

    remove incorrect energy tally parser, simplify the keff parser, rename some variables for clarity and adjust the tests

--- neutronics/parse_outputs.py
+++ neutronics/parse_outputs.py
@@ -10,7 +10,7 @@ import material_data as md
 
 # sampling parameters from neutronic_sweeps
 sampled_params = ns._dimensions
-res = ['keff', 'ave_E', 'mass', 'q_dens', 'BOL_U', 'EOL_U', 'rel_depl', 'dU']
+res = ['keff', 'mass', 'q_dens', 'BOL_U', 'EOL_U', 'rel_depl', 'dU']
 names = sampled_params + res
 types = ['f8']*len(names)
 
@@ -45,7 +45,7 @@ def parse_header_string(lines, data):
     Arguments:
     ----------
         lines (list): list of strings (one for each line)
-
+        data (ndarray): container to store results
     """
     for line in lines:
         if '1-' in line:
@@ -56,80 +56,21 @@ def parse_header_string(lines, data):
             break
 
 def parse_keff(lines, data):
-    """Parse the keff data from the output file.
+    """Parse the EOL keff from the output file.
     
     Arguments:
     ----------
         lines (list): lines of the output file (to be parsed)
         data (ndarray): container to store results
     """
-    keff = []
-    err = []
-    days = []
-    BU = []
     res_loc = []
     
     depletion_table_offset = 8
     for idx, line in enumerate(lines):
         if 'final result' in line:
             res_loc.append(idx)
-        if 'print table 210' in line:
-            burndx = idx + depletion_table_offset
 
-    # skip the predictor calcs 
-    save_res = res_loc[0::2]
-
-    for line_num in save_res:
-        keff.append(float(lines[line_num].split()[2]))
-        err.append(float(lines[line_num].split()[3]))
-
-    for burndata in lines[burndx:]:
-        if burndata == '\n':
-            break
-        BU.append(float(burndata.split()[8]))
-        days.append(float(burndata.split()[2]))
-    
-    data['keff'] = keff[-1]
-
-
-def parse_etal(lines, data):
-    """Parse energy tallies from the output file.
-    Arguments:
-    ----------
-        tally (str): tally number
-        lines (list): lines of the output file (to be parsed)
-        data (ndarray): container to store results
-    """
-    bins = []
-    vals = []
-    errs = []
-    tally_locations = []
-    # get number of energy bins
-    bupper = lines.index(' energy bins\n')
-    blower = lines.index('      total bin\n')
-    nbins = blower - bupper
-    
-    for idx, line in enumerate(lines):
-        if '1tally' in line and 'nps' in line:
-            tally_locations.append(idx + 11)
-            
-    for tally in tally_locations:
-        bindata = []
-        valdata = []
-        errdata = []
-        for idx in range(tally, tally + nbins - 1):
-            bindata.append(float(lines[idx].split()[0]))
-            valdata.append(float(lines[idx].split()[1]))
-            errdata.append(float(lines[idx].split()[2]))
-        
-        bins.append(bindata)
-        vals.append(valdata)
-        errs.append(errdata)
-    
-    bin_energies = bins[0][:-1] + np.diff(bins[0]) / 2
-    # calculate average neutron energy
-    average = np.sum(bin_energies * vals[0][1:]) / sum(vals[0][1:])
-    data['ave_E'] = average
+    data['keff'] = float(lines[res_loc[-1]].split()[2])
 
 def calc_fuel_mass_q_dens(data):
     """Calculate the reactor fuel mass and coolant density.
@@ -139,6 +80,7 @@ def calc_fuel_mass_q_dens(data):
         data (ndarray): container to store results
     """
     core_r, r, PD, Q = data[['core_r', 'cool_r', 'PD', 'power']]
+    # clad thickness
     c = 0.0031
     l = core_r*data['AR']
     core_v = math.pi*core_r*core_r*l
@@ -223,7 +165,7 @@ def save_store_data(data_dir='./results/*o'):
 
     Arguments:
     ----------
-        data_dir (str): path to directory with MCNP6 output files
+        data_dir (str): glob string pointing to dir with output files
     """
     # gather files
     files = glob.glob(data_dir)
@@ -235,15 +177,14 @@ def save_store_data(data_dir='./results/*o'):
         print(file)
         
         fp = open(file, 'r')
-        string = fp.readlines()
+        output_lines = fp.readlines()
         fp.close()
         
         # get sampled parameters from header string
-        parse_header_string(string, data[idx])
+        parse_header_string(output_lines, data[idx])
         # parse results from MCNP6 output file
-        parse_keff(string, data[idx])
-        parse_etal(string, data[idx])
-        act_inv = parse_actinide_inventory(string)
+        parse_keff(output_lines, data[idx])
+        act_inv = parse_actinide_inventory(output_lines)
         # post-process the results
         calc_fuel_mass_q_dens(data[idx])
         depletion_analysis(act_inv, data[idx], file)
