51d0090ae6d501daafea7b59c134477f06565016
Alexander Swenson <aaswenson@wisc.edu>
2018-10-22 14:25:44 -0500

    cleaning up printouts

--- optimization/reactor_mass.py
+++ optimization/reactor_mass.py
@@ -34,23 +34,17 @@ def reactor_mass(fuel, coolant, power, m_dot, T, P,
     rxtr = Flow(cool_r, clad_t, AR, power, fuel, coolant, clad, refl, flow_props)
     # perform 1D calculation
     oned_flow_modeling(rxtr)
-    print(rxtr.gen_Q)
 
     return rxtr.mass
 
-Q_therm = 200000
-m_dot = 1
+Q_therm = 90000
+m_dot = 1.12
 
-print('UO2 CO2')
 test_mass = reactor_mass('UO2', 'CO2', Q_therm, m_dot, (900,1100), (1.79e7, 1.73e7))
-print(test_mass)
-print('UO2 H2O')
-test_mass = reactor_mass('UO2', 'H2O', Q_therm, m_dot, (900,1100), (1.79e7, 1.73e7))
-print(test_mass)
-print('UW CO2')
+print('UO2 CO2', test_mass)
+test_mass = reactor_mass('UO2', 'H2O', Q_therm, m_dot, (900,1100), (4.84e7, 4.77e7))
+print('UO2 H2O', test_mass)
 test_mass = reactor_mass('UW', 'CO2', Q_therm, m_dot, (900,1100), (1.79e7, 1.73e7))
-print(test_mass)
-print('UW H2O')
-test_mass = reactor_mass('UW', 'H2O', Q_therm, m_dot, (900,1100), (1.79e7, 1.73e7))
-print(test_mass)
-
+print('UW CO2', test_mass)
+test_mass = reactor_mass('UW', 'H2O', Q_therm, m_dot, (900,1100), (4.84e7, 4.77e7))
+print('UW H2O', test_mass)
